2e60443a84ad7b633989466b4d77395a37a3535b
Royanw <141390425+Royanw@users.noreply.github.com>
2023-10-13 10:48:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded",
+function () {
+    const productlist = document.getElementById("productsList");
+    const products = [
+        {name: "Product 1",price: "$10" , image:"img1.jpg" },
+            {name: "Product 1",price: "$30" , image:"img2.jpg" },
+           { name: "Product 1",price: "$160" , image:"img3.jpg" },
+        
+    ];
+
+
+    products.forEach((product) => {
+        const productElement = document.createElement("div");
+        productElement.className="product";
+
+        const imageElement = document.createElement("img");
+        imageElement.src= product.image;
+        imageElement.alt= product.name;
+         
+        const nameElement = document.createElement("h3");
+        nameElement.textContent =  product.name;
+        const priceElement = document.createElement("p");
+        priceElement.textContent = product.price;
+        
+        productElement.appendChild(imageElement);
+        productElement.appendChild(nameElement);
+        productElement.appendChild(priceElement);
+
+        productlist.appendChild(productElement);
+
+    }); 
+}
+)
+    
